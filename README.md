ae93e91f5f75357eec4125eacb5a6097d6627538
Brian Kramer <subsoap@gmail.com>
2018-05-24 16:05:38 -0700

    human type, readme

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Idle
 Toolkit useful for making incremental / idle games with Defold
+
+## Installing
+Library
+```https://github.com/subsoap/idle/archive/master.zip```
+Using
+```local idle = require("idle.idle")```
+
+### idle.display_number(num, type)
+
+```num``` is the input number, ```type``` is the suffix kind
+* type 1 = K,M,B,T,Qa,Qt ...
+* type 2 = K,M,B,T,aa,bb ...
+* type 3 = thousand, million, billion, trillion ...
+
+Examples
+
+```
+num = 1000000
+print(idle.display_number(num)) --> 1 M
+```
+
+```
+num = 1000000
+print(idle.display_number(num, 3)) --> 1 million 
+```
--- idle/idle.lua
+++ idle/idle.lua
@@ -10,8 +10,24 @@ function M.display_number(num, type)
 	{"", " K", " M", " B", " T", "aa", "bb", "cc", "dd", "ee", "ff", "gg", "hh", "ii", "jj", "kk", "ll", "mm", "nn", "oo", "pp", "qq",
 	"rr", "ss", "tt", "uu", "vv", "ww", "xx", "yy", "zz", "Aa", "Bb", "Cc", "Dd", "Ee", "Ff", "Gg", "Hh", "Ii", "Jj", "Kk", "Ll", "Mm", "Nn", "Oo", "Pp", 
 	"Qq", "Rr", "Ss", "Tt", "Uu", "Vv", "Ww", "Xx", "Yy", "Zz", "AA", "BB", "CC", "DD", "EE", "FF", "GG", "HH", "II", "JJ", "KK", "LL", "MM", "NN", "OO", 
-	"PP", "QQ", "RR", "SS", "TT", "UU", "VV", "WW", "XX", "YY", "ZZ"} 
-	}
+	"PP", "QQ", "RR", "SS", "TT", "UU", "VV", "WW", "XX", "YY", "ZZ"},
+	{"", " thousand ", " million ", " billion ", " trillion ", " quadrillion ", " quintillion ", " sextillion ", " septillion ", " octillion ", " nonillion ", 
+	" decillion ", " undecillion ", " duodecillion ", " tredecillion ", " quattuordecillion ", " quindecillion ", " sedecillion ", " septendecillion ", 
+	" octodecillion ", " novemdecillion  ", " vigintillion ", " unvigintillion ", " duovigintillion ", " trevigintillion ", " quattuorvigintillion ", 
+	" quinvigintillion ", " sexvigintillion ", " septenvigintillion ", " octovigintillion ", " novemvigintillion ", " trigintillion ", " untrigintillion ", 
+	" duotrigintillion ", " tretrigintillion ", " quattuortrigintillion ", " quintrigintillion ", " sextrigintillion ", " septentrigintillion ", 
+	" octotrigintillion ", " novemtrigintillion ", " quadragintillion ", " unquadragintillion ", " duoquadragintillion ", " trequadragintillion ", 
+	" quattuorquadragintillion ", " quinquadragintillion ", " sexquadragintillion ", " septenquadragintillion ", " octoquadragintillion ", 
+	" novemquadragintillion ", " quinquagintillion ", " unquinquagintillion ", " duoquinquagintillion ", " trequinquagintillion ", " quattuorquinquagintillion ", 
+	" quinquinquagintillion ", " sexquinquagintillion ", " septenquinquagintillion ", " octoquinquagintillion ", " novemquinquagintillion ", " sexagintillion ", 
+	" unsexagintillion ", " duosexagintillion ", " tresexagintillion ", " quattuorsexagintillion ", " quinsexagintillion ", " sexsexagintillion ", 
+	" septsexagintillion ", " octosexagintillion ", " octosexagintillion ", " septuagintillion ", " unseptuagintillion ", " duoseptuagintillion ", 
+	" treseptuagintillion ", " quinseptuagintillion"," sexseptuagintillion"," septseptuagintillion"," octoseptuagintillion"," novemseptuagintillion",
+	" octogintillion"," unoctogintillion"," duooctogintillion"," treoctogintillion"," quattuoroctogintillion"," quinoctogintillion"," sexoctogintillion",
+	" septoctogintillion"," octooctogintillion"," novemoctogintillion"," nonagintillion"," unnonagintillion"," duononagintillion", " trenonagintillion ",
+	" quattuornonagintillion "," quinnonagintillion "," sexnonagintillion "," septnonagintillion "," octononagintillion "," novemnonagintillion ", " centillion"}
+}
+
 
 	local place = 1
 
@@ -23,7 +39,7 @@ function M.display_number(num, type)
 	num = math.floor(num * 10 ) / 10
 
 	if suffixes[type][place] == nil then 
-		return tostring(num) .. "??"
+		return tostring(num) .. " I+" .. tostring(place - #suffixes)
 	end
 	
 	return tostring(num) .. suffixes[type][place]
